fb0520f68df81d1df5e03fe9530d53c001885f7a
first_name <ss329222@gmail.com>
2022-02-17 20:45:23 +0530

    Added:WelcomeMessage

---
+++ src/com/blz/practiceusingoops/EmployeePrescenceCheck.java
@@ -0,0 +1,26 @@
+package com.blz.practiceusingoops;
+
+import java.util.Random;
+
+class Employee {
+	static final int IS_FULL_TIME = 1;
+
+	public static void empCheckPrescence() {
+		Random rand = new Random();
+		int empCheck = rand.nextInt(2);
+		if (empCheck == IS_FULL_TIME) {
+			System.out.println("Employee is Present");
+		} else {
+			System.out.println("Employee is absent ");
+		}
+	}
+
+	public class EmployeePrescenceCheck {
+
+		public static void main(String[] args) {
+			System.out.println("Welcome to Employee Wage Computation Program on Master Branch");
+			Employee.empCheckPrescence();
+		}
+	}
+
+}
