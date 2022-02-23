ab2e021c6e01670b9cd3463e58b15e252ad15162
first_name <ss329222@gmail.com>
2022-02-23 06:38:15 +0530

    Added:EmployeeWageForMonth

--- src/com/blz/practiceusingoops/EmployeeWageComputation.java
+++ src/com/blz/practiceusingoops/EmployeeWageComputation.java
@@ -6,8 +6,7 @@ class Employe {
 	static final int IS_FULL_TIME = 1;
 	static final int IS_PART_TIME = 2;
 	static final int EMP_WAGE_PER_DAY = 20;
-
-	// generating random numbers with in 3 using random functions
+	static final int EMP_WAGE_FOR_MONTH = 20;
 
 	public static int empCheck() {
 		Random random = new Random();
@@ -15,7 +14,7 @@ class Employe {
 		return empCheck;
 	}
 
-	public static void empSalary(int empCheck) {
+	public static int empSalary(int empCheck) {
 		int empWrkHrs;
 		switch (empCheck) {
 		case IS_FULL_TIME -> empWrkHrs = 8;
@@ -23,16 +22,32 @@ class Employe {
 		default -> empWrkHrs = 0;
 		}
 		int empWage = empWrkHrs * EMP_WAGE_PER_DAY;
-		System.out.println("empWage = " + empWage);
+		return empWage;
 	}
+
+	public static int totalEmpSalary() {
+		int day = 0;
+		int totalEmpSalary = 0;
+		int empWage = 0;
+		int empCheck = 0;
+		while (day < EMP_WAGE_FOR_MONTH) {
+			empCheck = empCheck();
+			empWage = empSalary(empCheck);
+			++day;
+			totalEmpSalary += empWage;
+		}
+		return totalEmpSalary;
+	}
+
 }
 
 public class EmployeeWageComputation {
 
 	public static void main(String[] args) {
+		int empCheck, empWage, totalEmpSalary;
 		System.out.println("Welcome to Employee Wage Computation Program on Master Branch");
-		int empCheck = Employe.empCheck();
-		Employe.empSalary(empCheck);
+		totalEmpSalary = Employe.totalEmpSalary();
+		System.out.println("totalEmpSalary = " + totalEmpSalary);
 	}
 
 }
