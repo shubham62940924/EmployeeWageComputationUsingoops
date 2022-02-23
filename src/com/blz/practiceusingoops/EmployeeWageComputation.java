01b50162e7871974f4f692fa0e0019f58be7e629
first_name <ss329222@gmail.com>
2022-02-23 06:49:44 +0530

    Added:ApplyingOops

--- src/com/blz/practiceusingoops/EmployeeWageComputation.java
+++ src/com/blz/practiceusingoops/EmployeeWageComputation.java
@@ -2,61 +2,57 @@ package com.blz.practiceusingoops;
 
 import java.util.Random;
 
-class Employe {
-	static final int IS_FULL_TIME = 1;
-	static final int IS_PART_TIME = 2;
-	static final int EMP_WAGE_PER_DAY = 20;
-	static final int EMP_WAGE_FOR_MONTH = 20;
-	static final int TOTAL_WRK_HRS_MONTH = 100;
-
-	public static int empCheck() {
-		Random random = new Random();
-		int empCheck = random.nextInt(3);
-		return empCheck;
-	}
-
-	public static int empWrkHrs(int empCheck) {
-		int empWrkHrs;
-		switch (empCheck) {
-		case IS_FULL_TIME -> empWrkHrs = 8;
-		case IS_PART_TIME -> empWrkHrs = 4;
-		default -> empWrkHrs = 0;
-		}
-		return empWrkHrs;
-	}
-
-	public static int empWage(int empWrkHrs) {
-		int empWage = empWrkHrs * EMP_WAGE_PER_DAY;
-		return empWage;
-	}
-
-	public static int totalEmpSalary() {
-		int day = 0;
-		int totalEmpSalary = 0;
-		int empWrkHrs = 0;
-		int empWage = 0;
-		int empCheck = 0;
-		int totalEmpWrkHrs = 0;
-		while (day < EMP_WAGE_FOR_MONTH && totalEmpWrkHrs < TOTAL_WRK_HRS_MONTH) {
-			empCheck = empCheck();
-			empWrkHrs = empWrkHrs(empCheck);
-			empWage = empWage(empWrkHrs);
-			totalEmpWrkHrs += empWrkHrs;
-			++day;
-			totalEmpSalary += empWage;
-		}
-		return totalEmpSalary;
-	}
-
-}
-
 public class EmployeeWageComputation {
+	public static final int IS_PART_TIME = 1;
+	public static final int IS_FULL_TIME = 2;
+
+	private final String company;
+	private final int empRatePerHour;
+	private final int numOfWorkingDays;
+	private final int maxHoursPerMonth;
+
+	public EmployeeWageComputation(String company, int empRatePerHour, int numOfWorkingDays, int maxHoursPerMonth) {
+		super();
+		this.company = company;
+		this.empRatePerHour = empRatePerHour;
+		this.numOfWorkingDays = numOfWorkingDays;
+		this.maxHoursPerMonth = maxHoursPerMonth;
+	}
+
+	public static void totalEmpWage() {
+		EmployeeWageComputation dMart = new EmployeeWageComputation("dmart", 20, 20, 100);
+		Random random = new Random();
+		int totalWorkingDays = 0;
+		int totalWorkingHours = 0;
+		int empWage = 0;
+		int totalEmpWage = 0;
+
+		while (totalWorkingDays < dMart.numOfWorkingDays && totalWorkingHours < dMart.maxHoursPerMonth) {
+			totalWorkingDays++;
+			int empCheck = random.nextInt(3);
+			int empHrs = 0;
+			switch (empCheck) {
+
+			case IS_PART_TIME -> empHrs = 4;
+
+			case IS_FULL_TIME -> empHrs = 8;
+
+			default -> empHrs = 0;
+
+			}
+			totalWorkingHours = empHrs + totalWorkingHours;
+			empWage = empHrs * dMart.empRatePerHour;
+			System.out.println("day " + totalWorkingDays + " = " + empWage);
+			totalEmpWage = empWage + totalEmpWage;
+
+		}
+		System.out.println("salary of " + dMart.company + " employee is " + totalEmpWage);
+
+	}
 
 	public static void main(String[] args) {
-		int totalEmpSalary;
-		System.out.println("Welcome to Employee Wage  Computation Program on Master Branch");
-		totalEmpSalary = Employe.totalEmpSalary();
-		System.out.println("totalEmpSalary = " + totalEmpSalary);
+		totalEmpWage();
+
 	}
 
 }
