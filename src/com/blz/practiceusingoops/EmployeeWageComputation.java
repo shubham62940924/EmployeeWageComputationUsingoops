f9f7c743be2547ea7fde8b3f4b5f0d87ddfff4fc
first_name <ss329222@gmail.com>
2022-02-23 06:34:11 +0530

    Added:UsingSwitchCaseStatement

--- src/com/blz/practiceusingoops/EmployeeWageComputation.java
+++ src/com/blz/practiceusingoops/EmployeeWageComputation.java
@@ -2,10 +2,12 @@ package com.blz.practiceusingoops;
 
 import java.util.Random;
 
-class Employee {
+class Employe {
 	static final int IS_FULL_TIME = 1;
 	static final int IS_PART_TIME = 2;
 	static final int EMP_WAGE_PER_DAY = 20;
+
+	// generating random numbers with in 3 using random functions
 
 	public static int empCheck() {
 		Random random = new Random();
@@ -14,13 +16,11 @@ class Employee {
 	}
 
 	public static void empSalary(int empCheck) {
-		int empWrkHrs = 0;
-		if (empCheck == IS_FULL_TIME) {
-			empWrkHrs = 8;
-		} else if (empCheck == IS_PART_TIME) {
-			empWrkHrs = 4;
-		} else {
-			empWrkHrs = 0;
+		int empWrkHrs;
+		switch (empCheck) {
+		case IS_FULL_TIME -> empWrkHrs = 8;
+		case IS_PART_TIME -> empWrkHrs = 4;
+		default -> empWrkHrs = 0;
 		}
 		int empWage = empWrkHrs * EMP_WAGE_PER_DAY;
 		System.out.println("empWage = " + empWage);
@@ -31,8 +31,8 @@ public class EmployeeWageComputation {
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to Employee Wage Computation Program on Master Branch");
-		int empCheck = Employee.empCheck();
-		Employee.empSalary(empCheck);
+		int empCheck = Employe.empCheck();
+		Employe.empSalary(empCheck);
 	}
 
 }
